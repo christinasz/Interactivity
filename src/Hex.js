6ded4009dfbcc523bd9da9dedb1dddbbcceffbd6
christinasz <christinaszhang@gmail.com>
2017-05-17 22:17:52 -0400

    Initial commit

---
+++ src/Hex.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './Hex.css';
+
+// Returns a random hex code, corresponding to a colour
+function randomHex() {
+    var hex = "#";
+     // only including part of possible hex codes, so that it's all pretty pastels
+    var alphabet = "9ABCDEF";
+    for (var i = 0; i < 6; i++) {
+        hex += alphabet.charAt(Math.floor(Math.random() * 7));
+    }
+   // console.log(hex);
+    return hex;
+}
+
+class Hex extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {colour: randomHex()};
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick(e) {
+        e.preventDefault();
+        this.setState({colour: randomHex()});
+    }
+    render() {
+      return(
+        <div className="full" style={{backgroundColor: this.state.colour}} onClick={this.handleClick}>      
+        </div>
+      );  
+    }
+}
+                
+export default Hex;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Hex from './Hex';
+import './index.css';
+
+ReactDOM.render(
+  <Hex />,
+  document.getElementById('root')
+);
